0e278380edfffe1c3535dbe237a8b25f96f6e8d3
egoistic05 <vishnu.narayanan0510@gmail.com>
2025-05-12 21:40:00 +0530

    Fix: Image loading in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['randomuser.me'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'randomuser.me',
+        port: '',
+        pathname: '/api/portraits/**',
       },
     ],
+    unoptimized: true,
   },
 };
 
